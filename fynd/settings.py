5b745e5134b0d908dd5b4ee38356cc17e7903992
saket <saket.j14@gmail.com>
2015-12-28 09:38:12 +0530

    checking

--- fynd/settings.py
+++ fynd/settings.py
@@ -157,7 +157,6 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "imgui/static"),
 ]
